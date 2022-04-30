85ffbd17702d43563f22af022a3d003c22d61528
ericericericjin <jyseric@126.com>
2022-04-30 12:28:54 +0800

    Bug fix.

--- src/ui.cpp
+++ src/ui.cpp
@@ -265,21 +265,18 @@ namespace game
             }
 
             // player
-            // mvwaddstr(game_win, (int)round(player->get_yx().first + 1), (int)round(player->get_yx().second + 1), player->get_char().c_str());
-            mvwaddch(game_win, (int)round(player->get_yx().first + 1), (int)round(player->get_yx().second + 1), 'P');
+            mvwaddstr(game_win, (int)round(player->get_yx().first + 1), (int)round(player->get_yx().second + 1), player->get_char().c_str());
 
             // zombie
             for (auto &zombie : *zombie_list)
             {
-                // mvwaddstr(game_win, (int)round(zombie->get_yx().first) + 1, (int)round(zombie->get_yx().second) + 1, zombie->get_char().c_str());
-                mvwaddch(game_win, (int)round(zombie->get_yx().first) + 1, (int)round(zombie->get_yx().second) + 1, 'Z');
+                mvwaddstr(game_win, (int)round(zombie->get_yx().first) + 1, (int)round(zombie->get_yx().second) + 1, zombie->get_char().c_str());
             }
 
             // bullet
             for (auto &bullet : *bullet_list)
             {
-                // mvwaddstr(game_win, (int)round(bullet->get_yx().first) + 1, (int)round(bullet->get_yx().second) + 1, bullet->get_char().c_str());
-                mvwaddch(game_win, (int)round(bullet->get_yx().first) + 1, (int)round(bullet->get_yx().second) + 1, '*');
+                mvwaddstr(game_win, (int)round(bullet->get_yx().first) + 1, (int)round(bullet->get_yx().second) + 1, bullet->get_char().c_str());
             }
 
             wrefresh(game_win);
